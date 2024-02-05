cd3e52b12c55a8c3de34924e6de95e603e7244c1
Divyanshu Kumar <divyanshuk10@gmail.com>
2024-02-05 21:59:40 +0530

    initial commit

---
+++ app/src/main/java/com/divyanshu/musicplayerapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.divyanshu.musicplayerapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.divyanshu.musicplayerapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
